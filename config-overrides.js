2b8f458ce2eccabf12fa87c2ba73dcb1bd6e4d8a
zlatan <zlatan.pham@gmail.com>
2020-05-10 01:06:55 +0700

    chore: add less loader + dayjs replacement for moment

--- config-overrides.js
+++ config-overrides.js
@@ -7,9 +7,13 @@ const {
   addBundleVisualizer,
   fixBabelImports,
   addLessLoader,
+  addWebpackPlugin,
 } = require('customize-cra');
 
+const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin');
+
 module.exports = override(
+  addWebpackPlugin(new AntdDayjsWebpackPlugin()),
   fixBabelImports('antd', {
     libraryDirectory: 'es',
     style: 'css',
